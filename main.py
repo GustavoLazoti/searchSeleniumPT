27c958a3726b4418abdec5a33225f4d846bc827b
GustavoLazoti <gustavo.lazotti@hotmail.com>
2025-04-19 18:52:44 -0300

    Created main and basic functions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
 
 def clicar_letra(driver, letra, timeout=10):
     """
@@ -14,3 +15,13 @@ def clicar_letra(driver, letra, timeout=10):
         EC.element_to_be_clickable((By.XPATH, xpath))
     )
     botao.click()
+
+
+def main():
+    driver = webdriver.Chrome()
+    try:
+        driver.get("URL_DA_PAGINA_DO_INDICE_DE_CURSOS")
+        clicar_letra(driver, 'B')
+        input("Pressione Enter para sair...")
+    finally:
+        driver.quit()
