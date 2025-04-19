017a825844ca7161b0d6208d6ad522891397a958
GustavoLazoti <gustavo.lazotti@hotmail.com>
2025-04-19 18:44:20 -0300

    Added clicar_letra function

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-print("hello")
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-print("hello2")
+def clicar_letra(driver, letra, timeout=10):
+    """
+    Clica no botão de letra no índice de cursos.
+    :param driver: Instância do Selenium WebDriver.
+    :param letra: Letra (char) a ser clicada, ex: 'B'.
+    :param timeout: Tempo máximo de espera pelo elemento.
+    """
+    xpath = f'//a[@href="indcurso.asp?letra={letra}" and @accesskey="{letra}"]'
+    botao = WebDriverWait(driver, timeout).until(
+        EC.element_to_be_clickable((By.XPATH, xpath))
+    )
+    botao.click()
